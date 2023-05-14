6a798a16be29336bb31b89482e1768d85ae2b1c6
Emete Gift <emeteokeoghene@gmail.com>
2023-05-14 16:35:56 +0100

    commit

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,14 @@
-pip install -r requirements.txt
-python3.10 manage.py collectstatic
+# Build the poject
+echo "Bulid the project..."
+python3.10 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
+
+echo "collect static..."
+python3.10 manage.py collectstatic --noinput --clear
+
+
+# pip install -r requirements.txt
+# python3.10 manage.py collectstatic
